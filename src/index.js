ec1cf988aaa6dfde5525a9d5eedee7a2f006396a
yanmei <hd_yangym@sina.com>
2019-01-16 10:43:59 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import _ from "lodash";
+import _ from "lodash";
 import printMe from "./print.js";
 import "./styles.css";
 import { cube } from "./math.js";
@@ -40,16 +40,35 @@ import { cube } from "./math.js";
 
 // return element;
 // }
-async function getComponent() {
-  var element = document.createElement("div");
-  const _ = await import(/* webpackChunkName: "lodash"*/ "lodash");
-  element.innerHTML = _.join(["Hello", "webpack", " "]);
+// async function getComponent() {
+//   var element = document.createElement("div");
+//   const _ = await import(/* webpackChunkName: "lodash"*/ "lodash");
+//   element.innerHTML = _.join(["Hello", "webpack", " "]);
+//   return element;
+// }
+// getComponent().then(component => {
+//   document.body.appendChild(component);
+// });
+function component(){
+  var element = document.createElement('div');
+  var button = document.createElement('button');
+  var br = document.createElement('br');
+
+  button.innerHTML = 'Click me and look at the console!';
+  element.innerHTML = _.join(['Hello','webpack'," "]);
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // 使用ES6模板的import()方法时，必须只想模块的.default值，因为它才是promise被处理后返回的实际的module对象
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    console.log("index.js 中import后返回的结果----", module);
+    var print = module.default;
+    print();
+  });
   return element;
 }
-getComponent().then(component => {
-  document.body.appendChild(component);
-});
 
+document.body.appendChild(component());
 // document.body.appendChild(component());
 // let element = component(); // 当print.js 改变重新渲染时，重新获取渲染的元素
 // document.body.appendChild(element);
--- src/print.js
+++ src/print.js
@@ -1,7 +1,9 @@
-export default function printMe() {
+console.log('The print.js module has loaded! See the network tab in dev tools...');
+export default () => {
   // console.error('I get called from print.js!');
   // cosnole.error('I get called from print.js!');
   // console.log("I get called from print.js!");
-  console.log("Updating print.js ...");
+  // console.log("Updating print.js ...");
+  console.log('Button Clicked: Here\'s "some text"!');
   // console.log("New update print.js ...");
 }
